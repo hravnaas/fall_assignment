2964491d649e549d5852f23a274e3fb64abf7fb0
Hans Ravnaas <hravnaas@hotmail.com>
2016-09-24 19:16:47 -0700

    stuff

--- apps/fall_assignment/views.py
+++ apps/fall_assignment/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.core.urlresolvers import reverse
+#from django.db.models import Sum
+from .models import User
+from ..login_reg.models import User
 
-# Create your views here.
+def index(request):
+    if "userID" not in request.session:
+        # Prevent user from going to the success page if not logged in.
+        return redirect(reverse('useradmin:index'))
+
+    # User is logged in. Get the current status of pokes.
+    context = {
+        "firstName" : User.objects.get(id = request.session['userID']).alias
+    }
+    return render(request, 'fall_assignment/index.html', context)
